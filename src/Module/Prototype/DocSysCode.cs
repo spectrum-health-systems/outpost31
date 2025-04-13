0df6e12e410fa61d538fddcd270ad1ab11feb361
Chris Banwarth <github@aprettycoolprogram.com>
2025-04-12 20:53:00 -0400

    25.4.0-development+250412

---
+++ src/Module/Prototype/DocSysCode.cs
@@ -0,0 +1,38 @@
+﻿// ██████  ██  ██ ██████ █████ ██████ █████  ██████   ██████  ███
+// ██  ██  ██  ██   ██   █████ ██  ██ ██████   ██        ███   ██
+// ██████  ██████   ██   ██    ██████  █████   ██     ██████   ██
+//                                     Core.Prototype.BlockDoc.cs
+
+// u250412_code
+// u250412_documentation
+
+/* #DEVNOTE#
+ * This is prototype code for blocking the "DOC" Avatar System Code from opening specified forms.
+ *
+ * To use this code, pass the "_pDocSysCodeDenyAccessToForm" Script Parameter to the Tingen Web Service.
+ */
+
+using System;
+using ScriptLinkStandard.Objects;
+
+namespace Outpost31.Module.Prototype
+{
+    public class DocSysCode
+    {
+        public static OptionObject2015 DenyAccessToForm(OptionObject2015 sentOptObj)
+        {
+            var msg = $"Access Denied{Environment.NewLine}" +
+                      Environment.NewLine +
+                     "Please access this data via the Console Widget Viewer.";
+
+            if (sentOptObj.SystemCode == "DOC")
+            {
+                return sentOptObj.ToReturnOptionObject(1, msg);
+            }
+            else
+            {
+                return sentOptObj.ToReturnOptionObject(0, "");
+            }
+        }
+    }
+}
---
+++ src/Module/Prototype/Run.cs
@@ -0,0 +1,29 @@
+﻿// ██████  ██  ██ ██████ █████ ██████ █████  ██████   ██████  ███
+// ██  ██  ██  ██   ██   █████ ██  ██ ██████   ██        ███   ██
+// ██████  ██████   ██   ██    ██████  █████   ██     ██████   ██
+//                                          Core.Prototype.Run.cs
+
+// u250412_code
+// u250412_documentation
+
+using ScriptLinkStandard.Objects;
+
+namespace Outpost31.Module.Prototype
+{
+    public class Run
+    {
+        public static OptionObject2015 Code(OptionObject2015 sentOptObj, string sentSlnkScriptParam)
+        {
+            if (sentSlnkScriptParam == "_pDocSysCodeDenyAccessToForm")
+            {
+                return DocSysCode.DenyAccessToForm(sentOptObj);
+            }
+            else
+            {
+                return sentOptObj.ToReturnOptionObject(0, "");
+            }
+        }
+    }
+
+
+}
