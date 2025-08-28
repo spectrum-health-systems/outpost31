09529a906b50f0cc35d8e6a8beb9327588359118
Chris Banwarth <github@aprettycoolprogram.com>
2025-08-28 11:57:19 -0400

    25.8-development+250828

--- src/Core/Avatar/AvtrEnvironment.cs
+++ src/Core/Avatar/AvtrEnvironment.cs
@@ -5,32 +5,29 @@
 // Copyright (c) A Pretty Cool Program. All rights reserved.
 // Licensed under the Apache 2.0 license.
 // -----------------------------------------------------------------------------
-// u250827_code
-// u250827_documentation
+// u250828_code
+// u250828_documentation
 // =============================================================================
-
-using System;
-using System.IO;
 
 namespace Outpost31.Core.Avatar
 {
     /// <summary>Represents the components of an Avatar Environment.</summary>
     /// <remarks>
-    ///   <para>
-    ///     The Avatar Environment represents the <see cref="AvtrSys"><b>Avatar System</b></see> and <see cref="AvtrSysCode"><b>Avatar System <i>Code</b></i></see>.<br/>
-    ///     <br/>
-    ///     While both contain the word<i>System</i>, they are not the same! It is important to understand the difference between the two.<br/>
-    ///   </para>
+    ///   The Avatar Environment represents the <see cref="AvtrSys"><b>Avatar System</b></see> and <see cref="AvtrSysCode"><b>Avatar System <i>Code</b></i></see>.<br/>
+    ///   <br/>
+    ///   While both contain the word<i>System</i>, they are not the same! It is important to understand the difference between the two.<br/>
     ///   <include file='AppData/XmlDoc/ProjectInfo.xml' path='TngnOpto/Class[@name="ProjectInfo"]/Callback/*'/>
     /// </remarks>
     internal class AvtrEnvironment
     {
         /// <summary>The Avatar <i>System</i> that the Tingen Web Service will interface with.</summary>
         /// <remarks>
-        ///   <para>
-        ///     It is <b><i>very important</i></b> that<c>AvtrSys</c> is set correctly, otherwise lots of bad things can *and will* happen!<br/>
-        ///     <include file='AppData/XmlDoc/Core.Avatar.xml' path='Core.Avatar/Class[@name="AvtrEnvironment"]/AvtrSys/*'/>
-        ///   </para>
+        ///   It is <b><i>very important</i></b> that <c>AvtrSys</c> is set correctly, otherwise lots of bad things can *and will* happen!<br/>
+        ///   <br/>
+        ///   Common Avatar Systems are: <c>UAT</c>, <c>LIVE</c>, <c>SBOX</c>, and<c> BUILD</c>. However, the Tingen Web Service is only<br/>
+        ///   able to interface with the <b><c>UAT</c></b> and <b><c> LIVE</c></b> Avatar Systems.<br/>
+        ///   <br/>
+        ///   <include file='AppData/XmlDoc/Core.Avatar.xml' path='Core.Avatar/Class[@name="AvtrEnvironment"]/AvtrSys/*'/>
         /// </remarks>
         /// <value>Valid AvtrSys values: <c>LIVE</c>, <c>UAT</c>.</value>
         public string AvtrSys { get; set; }
